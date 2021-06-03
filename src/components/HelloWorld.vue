080c008ef59893b5e72798daff08f81ef4ce59c3
Manoj <manojkumar@gmail.com>
2021-06-03 20:03:26 +0530

    initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,48 +1,188 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="resume-details">
+    <b-container>
+      <b-row>
+        <div class="ml-4 title-details">
+          Resume Management Tool
+        </div>
+      </b-row>
+      <b-row>
+        <b-col>
+          <hr class="new5" />
+        </b-col>
+      </b-row>
+      <b-row>
+        <div class="ml-4 heading-details">
+          User CV's
+        </div>
+      </b-row>
+      <b-row class="mt-4 ml-2">
+        <b-col>
+          <!-- Trigger the modal with a button -->
+          <b-card
+            title="Vincent"
+            img-src="/img/maleicon1.jpg"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 20rem;"
+            class="mb-2"
+          >
+            <b-card-text>
+              <b>Software Engineer</b><br />
+              <b> Texas</b>
+            </b-card-text>
+            <b-btn v-b-modal.stethescope class="mt-4 devicebtn"
+              >View Resume</b-btn
+            >
+          </b-card>
+        </b-col>
+        <b-col>
+          <!-- Trigger the modal with a button -->
+          <b-card
+            title="Harry"
+            img-src="/img/maleicon1.jpg"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 20rem;"
+            class="mb-2"
+          >
+            <b-card-text>
+              <b>Business Analyst</b><br />
+              <b> Florida</b>
+            </b-card-text>
+            <b-btn
+              variant="primary"
+              v-b-modal.stethescope
+              class="mt-4 devicebtn"
+              >View Resume</b-btn
+            >
+          </b-card>
+          <!-- Modal -->
+        </b-col>
+        <b-col>
+          <!-- Trigger the modal with a button -->
+          <b-card
+            title="Livingston"
+            img-src="/img/maleicon1.jpg"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 20rem;"
+            class="mb-2"
+          >
+            <b-card-text>
+              <b>Civil Engineer</b><br />
+              <b> Chicago</b>
+            </b-card-text>
+            <b-btn
+              variant="primary"
+              v-b-modal.stethescope
+              class="mt-4 devicebtn"
+              >View Resume</b-btn
+            >
+          </b-card>
+          <!-- Modal -->
+        </b-col>
+      </b-row>
+      <b-row class="mt-4">
+        <div class="ml-4 heading-details">
+          Upload CV
+        </div>
+      </b-row>
+      <b-row class="mr-2 ml-2 mt-4 mb-4">
+        <b-col lg="12">
+          <vue-dropzone
+            ref="dropzone"
+            id="drop1"
+            :options="dropOptions"
+          ></vue-dropzone>
+          <b-button class="mt-2" variant="primary" @click="removeAllFiles"
+            >Add File</b-button
+          >
+        </b-col>
+      </b-row>
+      <b-modal
+        id="stethescope"
+        size="lg"
+        ref="stethescope"
+        title="RESUME"
+        :hide-footer="true"
+      >
+        <div style="text-align:center ; cursor:pointer ; height:250px">
+          <img style="height:150px" alt="Vue logo" src="@/assets/pdf.png" />
+        </div>
+        <div style="float:right">
+          <b-btn @click="editResume">
+            Edit Resume ?
+          </b-btn>
+        </div>
+      </b-modal>
+    </b-container>
   </div>
 </template>
 
 <script>
+import vue2Dropzone from "vue2-dropzone";
+import "vue2-dropzone/dist/vue2Dropzone.min.css";
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  components: {
+    vueDropzone: vue2Dropzone,
+  },
+  data() {
+    return {
+      dropOptions: {
+        url: "https://httpbin.org/post",
+        maxFilesize: 2, // MB
+        maxFiles: 4,
+        chunking: true,
+        chunkSize: 500, // Bytes
+        thumbnailWidth: 150, // px
+        thumbnailHeight: 150,
+        addRemoveLinks: true,
+        acceptedFiles: "image/png,image/jpeg,application/pdf",
+      },
+      input: "",
+      input1: "",
+    };
+  },
+  methods: {
+    removeAllFiles() {
+      this.$alert("File added successfully");
+    },
+    editResume() {
+      this.$alert("Cannot edit file at the moment");
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
+}
+.heading-details {
+  font-size: 24px;
+  font-weight: 600;
+  color: grey;
+}
+.title-details {
+  font-size: 25px;
+  font-weight: 600;
+  color: grey;
+}
+.btn:not(:disabled):not(.disabled) {
+  cursor: pointer;
+  color: white;
+  background-color: #90b09e;
+  border-color: grey;
+  border-radius: 14px;
 }
 ul {
   list-style-type: none;
@@ -51,6 +191,10 @@ ul {
 li {
   display: inline-block;
   margin: 0 10px;
+}
+hr.new5 {
+  border: 8px solid #90b09e;
+  border-radius: 5px;
 }
 a {
   color: #42b983;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueSimpleAlert from "vue-simple-alert";
+
+Vue.use(VueSimpleAlert); 
+
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
 Vue.config.productionTip = false
 
 new Vue({
