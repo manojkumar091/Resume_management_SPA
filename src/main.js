9b2f0ac3eec75c184707aa98359da57cd361e956
Manoj <manojkumar@gmail.com>
2021-06-05 23:32:58 +0530

    [ADD] Buefy package

--- src/main.js
+++ src/main.js
@@ -5,8 +5,13 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import VueSimpleAlert from "vue-simple-alert";
+import Buefy from "buefy";
+import "buefy/dist/buefy.css";
 
 Vue.use(VueSimpleAlert); 
+Vue.use(Buefy, {
+  defaultIconPack: "fas",
+});
 
 
 // Make BootstrapVue available throughout your project
